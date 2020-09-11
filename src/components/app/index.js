8e2e6041671fa5812b0d8a09dc0e23a2f2895227
Lex <zelenskii1999@mail.ru>
2020-09-11 23:12:52 +0700

    build

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,13 +1,31 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import { Router, Route, Switch, Redirect } from "react-router-dom";
+import React, {
+  useEffect,
+  useState
+} from "react";
+import {
+  useDispatch
+} from "react-redux";
+import {
+  Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 import "./App.css";
-import { usersLoaded } from "../../redux/actions/user";
+import {
+  usersLoaded
+} from "../../redux/actions/user";
 import ErrorMessage from "../errorMessage";
 import Spinner from "../spinner";
-import { createBrowserHistory } from "history";
-import { ContactList } from "../user-list";
-import { ShowUser } from "../show-user";
+import {
+  createBrowserHistory
+} from "history";
+import {
+  ContactList
+} from "../user-list";
+import {
+  ShowUser
+} from "../show-user";
 import moment from "moment";
 
 function App() {
@@ -17,7 +35,7 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [contacts, setContacts] = useState(localStorage.getItem("contacts"));
   useEffect(() => {
-    if (contacts.length > 0) {
+    if (contacts) {
       dispatch(usersLoaded(JSON.parse(contacts)));
       setError(false);
       setLoading(false);
@@ -26,8 +44,7 @@ function App() {
     }
   }, [contacts]);
 
-  const tabBar = [
-    {
+  const tabBar = [{
       name: "Избранные",
     },
     {
@@ -60,66 +77,92 @@ function App() {
       });
   }
 
-  const errorMessage = error ? <ErrorMessage /> : null;
-  const spinner = loading ? <Spinner /> : null;
-  const content = !(loading || error) ? (
-    <Router history={history}>
-      <Switch>
-        <Route path="/" exact component={ContactList} />
-        <Route path="/user" exact component={ShowUser} />
-        <Redirect from="*" to="/" />
-      </Switch>
-    </Router>
+  const errorMessage = error ? < ErrorMessage / > : null;
+  const spinner = loading ? < Spinner / > : null;
+  const content = !(loading || error) ? ( <
+    Router history = {
+      history
+    } >
+    <
+    Switch >
+    <
+    Route path = "/"
+    exact component = {
+      ContactList
+    }
+    /> <
+    Route path = "/user"
+    exact component = {
+      ShowUser
+    }
+    /> <
+    Redirect from = "*"
+    to = "/" / >
+    <
+    /Switch> <
+    /Router>
   ) : null;
 
-  return (
-    <div className="App">
-      <div className="chelka">
-        <div
-          style={{
-            width: "20%",
-            height: "30%",
-            backgroundColor: "black",
-            borderRadius: 20,
-          }}
-        />
-      </div>
-      <div className="top">
-        <a className="time">{moment().format("HH:mm")}</a>
-      </div>
-      <header>
-        <h1 className="header">Контакты</h1>
-      </header>
-      {errorMessage}
-      {spinner}
-      {content}
-      <div
-        style={{
-          flexDirection: "row",
-          display: "flex",
-          justifyContent: "space-around",
-          width: "100%",
-          height: 68,
-          backgroundColor: "#969696",
-          borderBottomLeftRadius: 45,
-          borderBottomRightRadius: 45,
-          alignItems: "center",
-        }}
-      >
-        {/* {tabBar.map((item, index) => {
-          return (
-            <div
-              style={{
-                marginLeft: 10,
-              }}
-              key={index}
-            >
-              <a style={{ fontSize: 14 }}>{item.name}</a>
-            </div>
-          );
-        })} */}
-      </div>
-    </div>
+  return ( <
+    div className = "App" >
+    <
+    div className = "chelka" >
+    <
+    div style = {
+      {
+        width: "20%",
+        height: "30%",
+        backgroundColor: "black",
+        borderRadius: 20,
+      }
+    }
+    /> <
+    /div> <
+    div className = "top" >
+    <
+    a className = "time" > {
+      moment().format("HH:mm")
+    } < /a> <
+    /div> <
+    header >
+    <
+    h1 className = "header" > Контакты < /h1> <
+    /header> {
+      errorMessage
+    } {
+      spinner
+    } {
+      content
+    } <
+    div style = {
+      {
+        flexDirection: "row",
+        display: "flex",
+        justifyContent: "space-around",
+        width: "100%",
+        height: 68,
+        backgroundColor: "#969696",
+        borderBottomLeftRadius: 45,
+        borderBottomRightRadius: 45,
+        alignItems: "center",
+      }
+    } >
+    {
+      /* {tabBar.map((item, index) => {
+                return (
+                  <div
+                    style={{
+                      marginLeft: 10,
+                    }}
+                    key={index}
+                  >
+                    <a style={{ fontSize: 14 }}>{item.name}</a>
+                  </div>
+                );
+              })} */
+    } <
+    /div> <
+    /div>
   );
 }
 
