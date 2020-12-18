194c0aa621776e0628251b4020737fc5d8c3e616
Lilze <zelenskii1999@mail.ru>
2020-12-18 20:17:20 +0700

    Updates

--- precache-manifest.5d7eb1c7e7fc03e97e3038d0867664e9.js
+++ precache-manifest.5d7eb1c7e7fc03e97e3038d0867664e9.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "016889a09550416279390e35b2887b57",
+    "revision": "ab8d6b2be410b9d92b657e81090dfe3e",
     "url": "/labs-sibsutis/index.html"
   },
   {
-    "revision": "f37a7cd1e551ca7b42be",
+    "revision": "1890bfd1cb525e54213c",
     "url": "/labs-sibsutis/static/css/2.d9ad5f5c.chunk.css"
   },
   {
-    "revision": "f4b358acb3152bce327a",
+    "revision": "a63c9b5ce6131d05f9f8",
     "url": "/labs-sibsutis/static/css/main.49b7c5b3.chunk.css"
   },
   {
-    "revision": "f37a7cd1e551ca7b42be",
-    "url": "/labs-sibsutis/static/js/2.e86177b8.chunk.js"
+    "revision": "1890bfd1cb525e54213c",
+    "url": "/labs-sibsutis/static/js/2.3fa54e7c.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/labs-sibsutis/static/js/2.e86177b8.chunk.js.LICENSE.txt"
+    "url": "/labs-sibsutis/static/js/2.3fa54e7c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f4b358acb3152bce327a",
-    "url": "/labs-sibsutis/static/js/main.3122e166.chunk.js"
+    "revision": "a63c9b5ce6131d05f9f8",
+    "url": "/labs-sibsutis/static/js/main.33e683af.chunk.js"
   },
   {
     "revision": "2ef58c1fe2f9aae3a4f4",
